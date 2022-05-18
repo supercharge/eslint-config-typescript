cd1f6097748dbf4d8e117c4a94bebdebceb7b32d
Marcus Pöhls <mp@seda.digital>
2022-05-18 05:20:25 +0200

    update test

--- test/basic.js
+++ test/basic.js
@@ -6,8 +6,9 @@ const { test } = require('@japa/runner')
 const { isObject } = require('./helpers/utils')
 
 test('test basic properties of config', () => {
-  // expect(isObject(config.env)).toBe(true)
-  expect(isObject(config.rules)).toBe(true)
-  // expect(isObject(config.globals)).toBe(true)
-  expect(isObject(config.parserOptions)).toBe(true)
+  expect(config.extends).toEqual('@supercharge')
+
+  expect(isObject(config.overrides)).toBe(true)
+  expect(isObject(config.overrides[0].rules)).toBe(true)
+  expect(isObject(config.overrides[0].parserOptions)).toBe(true)
 })
